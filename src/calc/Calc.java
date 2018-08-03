7a4f7bb8e9ab2d8edf15418c2946108f7f1b02b8
jeisonroa1 <jeisonroa1@gmail.com>
2018-08-03 10:30:59 -0500

    Se implementa metodo multiplicacion. Las pruebas ahora son satisfactorias.

--- src/calc/Calc.java
+++ src/calc/Calc.java
@@ -98,7 +98,8 @@ public class Calc {
     }
     
     public static String multiplicacion(double a, double b){
-        String value = "0";
+        double values = a*b;
+        String value = String.format ("%.2f", values);
     return value;
         
     }
